8a707aeeb521d82fc8107d8e31275cfa434dbfd8
Ashley Mathew <ashley@aptible.com>
2020-09-16 11:08:03 -0400

    Remove codecov since it doesn't support all tested Ruby versions

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,14 +3,6 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 
 Bundler.require :development
 
-require 'simplecov'
-SimpleCov.start
-
-if ENV['CI']
-  require 'codecov'
-  SimpleCov.formatter = SimpleCov::Formatter::Codecov
-end
-
 # Load shared spec files
 Dir["#{File.dirname(__FILE__)}/shared/**/*.rb"].each do |file|
   require file
