475d70fb93aa5a6f500e6edaee038c16ca6b81e3
Josh Raker <48493233+joshraker@users.noreply.github.com>
2022-09-07 11:03:13 -0400

    Connect to running operation's logs (#307)
    * View live operation logs.
    
    * Sync readme usage.
    
    * Update command to operation:connect
    
    * Update tests
    
    * Update command description
    
    * Update command language
    
    * Update README
    
    Co-authored-by: Alex Kubacki <usernotfound@gmail.com>

--- spec/aptible/cli/subcommands/operation_spec.rb
+++ spec/aptible/cli/subcommands/operation_spec.rb
@@ -26,4 +26,54 @@ describe Aptible::CLI::Agent do
       subject.send('operation:cancel', 1)
     end
   end
+
+  describe '#operation:follow' do
+    it 'fails if the operation cannot be found' do
+      expect(Aptible::Api::Operation).to receive(:find).with(1, token: token)
+        .and_return(nil)
+
+      expect { subject.send('operation:follow', 1) }
+        .to raise_error('Operation #1 not found')
+    end
+
+    it 'connects to a running operation' do
+      op = Fabricate(:operation, status: 'running', type: 'restart')
+      expect(Aptible::Api::Operation).to receive(:find)
+        .with(op.id.to_s, token: token).and_return(op)
+
+      expect(subject).to receive(:attach_to_operation_logs).with(op)
+      subject.send('operation:follow', op.id.to_s)
+    end
+
+    it 'connects to a queued operation' do
+      op = Fabricate(:operation, status: 'queued', type: 'restart')
+      expect(Aptible::Api::Operation).to receive(:find)
+        .with(op.id.to_s, token: token).and_return(op)
+
+      expect(subject).to receive(:attach_to_operation_logs).with(op)
+      subject.send('operation:follow', op.id.to_s)
+    end
+
+    it 'does not connect to a failed operation' do
+      id = 34
+      status = 'failed'
+      op = Fabricate(:operation, id: id, status: status)
+      expect(Aptible::Api::Operation).to receive(:find)
+        .with(op.id.to_s, token: token).and_return(op)
+
+      expect { subject.send('operation:follow', op.id.to_s) }
+        .to raise_error(Thor::Error, /aptible operation:logs #{id}/)
+    end
+
+    it 'does not connect to a succeeded operation' do
+      id = 43
+      status = 'succeeded'
+      op = Fabricate(:operation, id: id, status: status)
+      expect(Aptible::Api::Operation).to receive(:find)
+        .with(op.id.to_s, token: token).and_return(op)
+
+      expect { subject.send('operation:follow', op.id.to_s) }
+        .to raise_error(Thor::Error, /aptible operation:logs #{id}/)
+    end
+  end
 end
