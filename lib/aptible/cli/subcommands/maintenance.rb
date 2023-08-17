789cd1740fbc9ea1acbaf069053ab22179ff30d8
Madhu <madhuravius@protonmail.com>
2023-08-17 10:04:01 -0400

    chore: cops

--- lib/aptible/cli/subcommands/maintenance.rb
+++ lib/aptible/cli/subcommands/maintenance.rb
@@ -7,27 +7,29 @@ module Aptible
             include Helpers::Maintenance
             include Helpers::Token
 
-            desc 'maintenance:apps', 'List Apps impacted by maintenance schedules'
+            desc 'maintenance:apps', 
+              'List Apps impacted by maintenance schedules'
             define_method 'maintenance:apps' do
               Formatter.render(Renderer.current) do |root|
                 root.list do |node|
                   maintenance_apps.each do |app|
                     next unless app.maintenance_deadline
                     node.object do |n|
-                      ResourceFormatter.inject_maintenance(n,app)
+                      ResourceFormatter.inject_maintenance(n, app)
                     end
                   end
                 end
               end
             end
-            desc 'maintenance:databases', 'List Databases impacted by maintenance schedules'
+            desc 'maintenance:databases',
+              'List Databases impacted by maintenance schedules'
             define_method 'maintenance:databases' do
               Formatter.render(Renderer.current) do |root|
                 root.list do |node|
                   maintenance_databases.each do |db|
                     next unless db.maintenance_deadline
                     node.object do |n|
-                      ResourceFormatter.inject_maintenance(n,db)
+                      ResourceFormatter.inject_maintenance(n, db)
                     end
                   end
                 end
