fc1e40859ee1e8b86d1d3d8bbc0403af788c6442
Alex Kubacki <usernotfound@gmail.com>
2023-08-28 12:35:19 -0600

    Fix date output for scheduled maintenance.

--- lib/aptible/cli/helpers/date_helpers.rb
+++ lib/aptible/cli/helpers/date_helpers.rb
@@ -2,6 +2,8 @@ module Aptible
   module CLI
     module Helpers
       module DateHelpers
+        # This should only be used by the method processing timestamps from S3 file names S3LogHelpers
+        # It is used to process a user-provided string into UTC.
         def utc_date(date_string)
           t_fmt = '%Y-%m-%d %Z'
           Time.strptime("#{date_string} UTC", t_fmt)
@@ -9,14 +11,19 @@ module Aptible
           raise Thor::Error, 'Please provide dates in YYYY-MM-DD format'
         end
 
+        # This should only be used by the method processing timestamps from S3 file names S3LogHelpers
+        # The file name does not include any time zone information, but we know it to be in UTC,
+        # so we add the "Z"  
         def utc_datetime(datetime_string)
           Time.parse("#{datetime_string}Z")
         rescue ArgumentError
           nil
         end
 
+        # This is used to format timestamps returned by our API into a more readable format.
+        # EG, "2023-09-05T22:00:00.000Z" returns "2023-09-05 22:00:00 UTC"
         def utc_string(datetime_string)
-          Time.parse("#{datetime_string}Z").utc
+          Time.parse("#{datetime_string}")
         rescue ArgumentError
           nil
         end
