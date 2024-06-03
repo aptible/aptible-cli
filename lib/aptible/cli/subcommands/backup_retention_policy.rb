235a73b146887620d969a882abd73b1fea59a7b8
Josh Raker <josh.raker@aptible.com>
2024-06-03 14:46:57 -0400

    Update backup_retention_policy:set usage

--- lib/aptible/cli/subcommands/backup_retention_policy.rb
+++ lib/aptible/cli/subcommands/backup_retention_policy.rb
@@ -17,8 +17,8 @@ module Aptible
               account = ensure_environment(environment: env)
               policy = account.backup_retention_policies.first
               unless policy
-                raise Thor::Error, 'Could not find backup retention policy ' \
-                                   "for environment #{env}"
+                raise Thor::Error, "Environment #{env} does not have a " \
+                                   'custom backup retention policy'
               end
 
               Formatter.render(Renderer.current) do |root|
@@ -34,40 +34,52 @@ module Aptible
                  '[--daily DAILY_BACKUPS] [--monthly MONTHLY_BACKUPS] ' \
                  '[--yearly YEARLY_BACKUPS] [--make-copy|--no-make-copy] ' \
                  '[--keep-final|--no-keep-final]',
-                 "Set the environemnt's backup retention policy"
+                 "Changes the environemnt's backup retention policy"
             option :daily, type: :numeric,
-                           desc: 'Number of daily backups to retain',
-                           default: 1
+                           desc: 'Number of daily backups to retain'
             option :monthly, type: :numeric,
-                             desc: 'Number of monthly backups to retain',
-                             default: 0
+                             desc: 'Number of monthly backups to retain'
             option :yearly, type: :numeric,
-                            desc: 'Number of yearly backups to retain',
-                            default: 0
+                            desc: 'Number of yearly backups to retain'
             option :make_copy, type: :boolean,
-                               desc: 'If backup copies should be created',
-                               default: false
+                               desc: 'If backup copies should be created'
             option(
               :keep_final,
               type: :boolean,
-              desc: 'If final backups should be kept when databases are '\
-                    'deprovisioned',
-              default: false
+              desc: 'If final backups should be kept when databases are ' \
+                    'deprovisioned'
             )
             define_method 'backup_retention_policy:set' do |env|
+              if options.empty?
+                raise Thor::Error,
+                      'Please specify at least one attribute to change'
+              end
+
               account = ensure_environment(environment: env)
-              policy = account.create_backup_retention_policy!(
-                daily: options[:daily],
-                monthly: options[:monthly],
-                yearly: options[:yearly],
-                make_copy: options[:make_copy],
-                keep_final: options[:keep_final]
-              )
+              current_policy = account.backup_retention_policies.first
+
+              # If an attribute isn't provided, use the value from the current
+              # policy
+              attrs = {}
+              %i[daily monthly yearly make_copy keep_final].each do |a|
+                opt = options[a]
+                attrs[a] = opt.nil? ? current_policy.try(a) : opt
+              end
+
+              # If any of the attribues are missing, raise an error so that
+              # we're not relying on the server's defaults
+              if attrs.values.any?(&:nil?)
+                raise Thor::Error, "Environemnt #{env} does not have a " \
+                                   'custom backup retention policy. Please ' \
+                                   'specify all attributes to create one.'
+              end
+
+              new_policy = account.create_backup_retention_policy!(**attrs)
 
               Formatter.render(Renderer.current) do |root|
                 root.object do |node|
                   ResourceFormatter.inject_backup_retention_policy(
-                    node, policy.reload, account
+                    node, new_policy.reload, account
                   )
                 end
               end
