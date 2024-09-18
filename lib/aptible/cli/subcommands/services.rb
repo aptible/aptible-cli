7c7948958ada03dd8aea8627e53f50b8431b6b83
Mario de la Ossa <mario.delaossa@aptible.com>
2024-09-18 13:25:47 -0600

    Add services:settings

--- lib/aptible/cli/subcommands/services.rb
+++ lib/aptible/cli/subcommands/services.rb
@@ -21,6 +21,27 @@ module Aptible
                 end
               end
             end
+
+            desc 'services:settings SERVICE',
+                 'Modify the zero-downtime deploy setting for a service'
+            app_options
+            option :force_zero_downtime,
+                   type: :boolean, default: false,
+                   desc: 'Force zero downtime deployments.'\
+                   ' Has no effect if service has an associated Endpoint'
+            option :simple_health_check,
+                   type: :boolean, default: false,
+                   desc: 'Use a simple uptime healthcheck during deployments'
+            define_method 'services:settings' do |service|
+              service = ensure_service(options, service)
+              updates = {}
+              updates[:force_zero_downtime] =
+                options[:force_zero_downtime] if options[:force_zero_downtime]
+              updates[:naive_health_check] =
+                options[:simple_health_check] if options[:simple_health_check]
+
+              service.update!(**updates) if updates.any?
+            end
           end
         end
       end
--- spec/aptible/cli/subcommands/services_spec.rb
+++ spec/aptible/cli/subcommands/services_spec.rb
@@ -8,45 +8,78 @@ describe Aptible::CLI::Agent do
     allow(subject).to receive(:fetch_token) { token }
     allow(Aptible::Api::App).to receive(:all).with(token: token)
       .and_return([app])
-    allow(subject).to receive(:options).and_return(app: app.handle)
   end
 
-  it 'lists a CMD service' do
-    Fabricate(:service, app: app, process_type: 'cmd', command: nil)
-    subject.send('services')
+  describe '#services' do
+    before do
+      allow(subject).to receive(:options).and_return(app: app.handle)
+    end
 
-    expect(captured_output_text.split("\n")).to include('Service: cmd')
-    expect(captured_output_text.split("\n")).to include('Command: CMD')
+    it 'lists a CMD service' do
+      Fabricate(:service, app: app, process_type: 'cmd', command: nil)
+      subject.send('services')
+
+      expect(captured_output_text.split("\n")).to include('Service: cmd')
+      expect(captured_output_text.split("\n")).to include('Command: CMD')
+    end
+
+    it 'lists a service with command' do
+      Fabricate(:service, app: app, process_type: 'cmd', command: 'foobar')
+      subject.send('services')
+
+      expect(captured_output_text.split("\n")).to include('Service: cmd')
+      expect(captured_output_text.split("\n")).to include('Command: foobar')
+    end
+
+    it 'lists container size' do
+      Fabricate(:service, app: app, container_memory_limit_mb: 1024)
+      subject.send('services')
+
+      expect(captured_output_text.split("\n"))
+        .to include('Container Size: 1024')
+    end
+
+    it 'lists container count' do
+      Fabricate(:service, app: app, container_count: 3)
+      subject.send('services')
+
+      expect(captured_output_text.split("\n")).to include('Container Count: 3')
+    end
+
+    it 'lists multiple services' do
+      Fabricate(:service, app: app, process_type: 'foo')
+      Fabricate(:service, app: app, process_type: 'bar')
+      subject.send('services')
+
+      expect(captured_output_text.split("\n")).to include('Service: foo')
+      expect(captured_output_text.split("\n")).to include('Service: bar')
+    end
   end
 
-  it 'lists a service with command' do
-    Fabricate(:service, app: app, process_type: 'cmd', command: 'foobar')
-    subject.send('services')
+  describe '#services:settings' do
+    let(:base_options) { { app: app.handle } }
 
-    expect(captured_output_text.split("\n")).to include('Service: cmd')
-    expect(captured_output_text.split("\n")).to include('Command: foobar')
+    it 'allows changing zero_downtime_deployment settings' do
+      stub_options(force_zero_downtime: true, simple_health_check: true)
+      service = Fabricate(:service, app: app, process_type: 'foo')
+
+      expect(service).to receive(:update!)
+        .with(force_zero_downtime: true, naive_health_check: true)
+
+      subject.send('services:settings', 'foo')
+    end
+
+    it 'allows changing only one of the options' do
+      stub_options(simple_health_check: true)
+      service = Fabricate(:service, app: app, process_type: 'foo')
+
+      expect(service).to receive(:update!).with(naive_health_check: true)
+
+      subject.send('services:settings', 'foo')
+    end
   end
 
-  it 'lists container size' do
-    Fabricate(:service, app: app, container_memory_limit_mb: 1024)
-    subject.send('services')
-
-    expect(captured_output_text.split("\n")).to include('Container Size: 1024')
-  end
-
-  it 'lists container count' do
-    Fabricate(:service, app: app, container_count: 3)
-    subject.send('services')
-
-    expect(captured_output_text.split("\n")).to include('Container Count: 3')
-  end
-
-  it 'lists multiple services' do
-    Fabricate(:service, app: app, process_type: 'foo')
-    Fabricate(:service, app: app, process_type: 'bar')
-    subject.send('services')
-
-    expect(captured_output_text.split("\n")).to include('Service: foo')
-    expect(captured_output_text.split("\n")).to include('Service: bar')
+  def stub_options(**opts)
+    allow(subject).to receive(:options).and_return(base_options.merge(opts))
   end
 end
