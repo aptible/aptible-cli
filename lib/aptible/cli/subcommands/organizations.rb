41610c6536102cbdc3c3cb8c40698914f4bebb4f
T Van Doren <210452321+tvdaptible@users.noreply.github.com>
2025-12-22 16:11:55 -0800

    simplify `aptible organizations` with user.roles_with_organizations call 😻

--- lib/aptible/cli/subcommands/organizations.rb
+++ lib/aptible/cli/subcommands/organizations.rb
@@ -10,53 +10,33 @@ module Aptible
             include Helpers::Telemetry
 
             desc 'organizations', 'List all organizations'
-            option :with_organization_role,
-                   aliases: '--with-org-role',
-                   type: :boolean,
-                   default: false,
-                   desc: 'Include your role in each organization ' \
-                         '(requires additional API calls)'
             def organizations
               telemetry(__method__, options)
 
-              token = fetch_token
-              orgs = Aptible::Auth::Organization.all(token: token)
-
-              user_roles_by_org = {}
-              if options[:with_organization_role]
-                user = Aptible::Auth::User.all(token: token).first
-                user.roles.each do |role|
-                  begin
-                    org = role.organization
-                  rescue StandardError
-                    next
-                  end
-                  next unless org
-
-                  org_id = org.id
-                  role_name = begin
-                                role.name
-                              rescue StandardError
-                                'unnamed'
-                              end
-
-                  user_roles_by_org[org_id] ||= []
-                  user_roles_by_org[org_id] << role_name
-                end
+              user = Aptible::Auth::Token.current_token(token: fetch_token).user
+              user_orgs_and_roles = {}
+              user.roles_with_organizations.each do |role|
+                user_orgs_and_roles[role.organization.id] ||= {
+                  'org' => role.organization,
+                  'roles' => []
+                }
+                user_orgs_and_roles[role.organization.id]['roles'] << role
               end
-
               Formatter.render(Renderer.current) do |root|
                 root.list do |list|
-                  orgs.each do |org|
+                  user_orgs_and_roles.each do |org_id, org_and_role|
+                    org = org_and_role['org']
+                    roles = org_and_role['roles']
                     list.object do |node|
                       node.value('id', org.id)
                       node.value('name', org.name)
-                      if org.respond_to?(:handle) && org.handle
-                        node.value('handle', org.handle)
-                      end
-                      if options[:with_organization_role]
-                        roles = user_roles_by_org[org.id] || []
-                        node.value('role', roles.join(', '))
+                      node.list('roles') do |roles_list|
+                        roles.each do |role|
+                          roles_list.object do |role_node|
+                            role_node.value('id', role.id)
+                            role_node.value('name', role.name)
+                          end
+                        end
                       end
                     end
                   end
